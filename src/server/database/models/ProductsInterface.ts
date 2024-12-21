076ccadfd143f32985b3b39703575f10eb52a21f
Danner Kurtz <kurtz.danner@gmail.com>
2024-12-21 09:04:30 -0300

    fix: adjusted getProduct method to include its related RawMaterials, fully functional

--- src/server/database/models/ProductsInterface.ts
+++ src/server/database/models/ProductsInterface.ts
@@ -8,10 +8,7 @@ export interface IProducts {
   percentage: number;
   price: number;
   quantity: number;
-<<<<<<< HEAD
   rawMaterialProductRelation?: IRawMaterialProductRelations[];
-=======
->>>>>>> 892223ee73e35a25c4a369de75d2a5a572616d86
   rawMaterials?: IRawMaterials[];
   createdAt?: Date;
   updatedAt?: Date;
