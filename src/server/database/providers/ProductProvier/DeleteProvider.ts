bfab9f8c012cb35c2ada7b32c5f697c6fc4fe105
Danner Kurtz <kurtz.danner@gmail.com>
2024-12-25 18:52:28 -0300

    feat: implement cascade delete for rawMaterialProductRelations when a product is deleted

--- src/server/database/providers/ProductProvier/DeleteProvider.ts
+++ src/server/database/providers/ProductProvier/DeleteProvider.ts
@@ -3,9 +3,26 @@ import {
   errorsCrudService,
   errorsProvider,
 } from '../../../shared/services/messageErrors';
+import { prisma } from '../../prisma';
 
 export const deleteProduct = async (id: string): Promise<Boolean | Error> => {
   try {
+    const getRawMaterialProductRelations =
+      await prisma.rawMaterialProductRelations.findMany({
+        where: { productId: id },
+      });
+
+    for (let i = 0; i < getRawMaterialProductRelations.length; i++) {
+      const deletedRawMaterialRelations = await crudService.deleteInDatabase(
+        getRawMaterialProductRelations[i].id,
+        'rawMaterialProductRelations',
+        errorsCrudService.deleteMessage('rawMaterialProductRelations')
+      );
+      if (deletedRawMaterialRelations instanceof Error) {
+        return new Error(deletedRawMaterialRelations.message);
+      }
+    }
+
     const deleteProduct: Boolean | Error = await crudService.deleteInDatabase(
       id,
       'Products',
