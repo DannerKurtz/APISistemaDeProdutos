d65f4261adc3e41e31c6e75671f07eb1f152a668
Danner Kurtz <kurtz.danner@gmail.com>
2024-12-26 16:14:35 -0300

    feat: Update product updates associated raw material relations

--- src/server/database/providers/ProductProvier/UpdateProvider.ts
+++ src/server/database/providers/ProductProvier/UpdateProvider.ts
@@ -4,13 +4,16 @@ import {
   errorsProvider,
 } from '../../../shared/services/messageErrors';
 import { IProducts, IProductsWithoutId } from '../../models/ProductsInterface';
+import { IRawMaterialProductRelations } from '../../models/RawMaterialProductRelationsInterface';
+import { prisma } from '../../prisma';
 
 export const update = async (
   id: string,
   body: IProductsWithoutId
 ): Promise<IProducts | Error> => {
   try {
-    const data: IProductsWithoutId = body;
+    const { rawMaterialProductRelation, ...data }: IProductsWithoutId = body;
+    const rawMaterialProductRelationList = [];
     const productUpdate: IProducts | Error = await crudService.updateInDatabase(
       id,
       data,
@@ -18,6 +21,29 @@ export const update = async (
       errorsCrudService.updateMessage('Products')
     );
 
+    if (productUpdate instanceof Error) return new Error(productUpdate.message);
+
+    if (!rawMaterialProductRelation) return productUpdate;
+
+    for (let i = 0; i < rawMaterialProductRelation.length; i++) {
+      const rawMaterialProductRelationGet =
+        await prisma.rawMaterialProductRelations.findMany({
+          where: {
+            productId: productUpdate.id,
+          },
+        });
+
+      for (let j = 0; j < rawMaterialProductRelationGet.length; j++) {
+        const rawMaterialProductRelationUpdate =
+          await prisma.rawMaterialProductRelations.update({
+            where: { id: rawMaterialProductRelationGet[j].id },
+            data: rawMaterialProductRelation[i],
+          });
+        rawMaterialProductRelationList.push(rawMaterialProductRelationUpdate);
+      }
+    }
+    productUpdate.rawMaterialProductRelation =
+      rawMaterialProductRelationList as IRawMaterialProductRelations[];
     return productUpdate;
   } catch (error) {
     return new Error(errorsProvider.updateMessage('Products'));
