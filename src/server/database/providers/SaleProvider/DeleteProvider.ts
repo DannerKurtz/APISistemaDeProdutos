95ee787d19ead6547a319b2a17df3d9d1eb17b68
Danner Kurtz <kurtz.danner@gmail.com>
2025-01-04 11:59:31 -0300

    feat: delete all relations when a sale is deleted
    Implemented functionality to automatically remove all related product-sale relations when a sale is deleted.
    - Updated deletion logic to cascade related entries.
    - Ensured database integrity after deletion.
    
    No breaking changes introduced.

--- src/server/database/providers/SaleProvider/DeleteProvider.ts
+++ src/server/database/providers/SaleProvider/DeleteProvider.ts
@@ -3,9 +3,25 @@ import {
   errorsCrudService,
   errorsProvider,
 } from '../../../shared/services/messageErrors';
+import { relationDelete } from '../../../shared/services/relationsManager/RelationDelete';
+import { prisma } from '../../prisma';
 
 export const deleteSale = async (id: string): Promise<Boolean | Error> => {
   try {
+    const getSaleProductRelations = await prisma.productSaleRelations.findMany({
+      where: { saleId: id },
+    });
+
+    for (let i = 0; i < getSaleProductRelations.length; i++) {
+      const deletedProductSaleRelations = await relationDelete(
+        getSaleProductRelations[i].id,
+        'ProductSaleRelations'
+      );
+      if (deletedProductSaleRelations instanceof Error) {
+        return new Error(deletedProductSaleRelations.message);
+      }
+    }
+
     const saleDeleted: Boolean | Error = await crudService.deleteInDatabase(
       id,
       'Sales',
