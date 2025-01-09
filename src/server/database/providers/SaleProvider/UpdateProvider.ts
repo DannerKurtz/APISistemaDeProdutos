5b8bebc33fdb9284790f3d4e1b0b3c84fbdb0180
Danner Kurtz <kurtz.danner@gmail.com>
2025-01-09 08:55:50 -0300

    feat: update product-sale relations when updating a sale
    Implemented functionality to automatically update the relations between products and sales when a sale is updated.
    - Ensured database consistency during the update process.
    - Modified the sales update logic to handle related product adjustments.
    
    No breaking changes introduced.

--- src/server/database/providers/SaleProvider/UpdateProvider.ts
+++ src/server/database/providers/SaleProvider/UpdateProvider.ts
@@ -3,18 +3,32 @@ import {
   errorsCrudService,
   errorsProvider,
 } from '../../../shared/services/messageErrors';
+import { updateRelations } from '../../../shared/services/relationsManager/RelationUpdate';
+import { IProductSaleRelations } from '../../models/ProductSaleRelationsInterface';
 import { ISalesWithoutId } from '../../models/SalesInterface';
 
-export const update = async (id: string, body: ISalesWithoutId) => {
+export const update = async (id: string, data: ISalesWithoutId) => {
   try {
-    const updateDateSale = await crudService.updateInDatabase(
+    const { productSaleRelations, ...dataWithoutProductSaleRelations } = data;
+    const updateSale = await crudService.updateInDatabase(
       id,
-      body,
+      dataWithoutProductSaleRelations,
       'Sales',
       errorsCrudService.updateMessage('Sales')
     );
 
-    return updateDateSale;
+    const relationUpdated = await updateRelations(
+      'ProductSaleRelations',
+      {
+        saleId: updateSale.id,
+      },
+      productSaleRelations as IProductSaleRelations[]
+    );
+
+    data = updateSale;
+    data.productSaleRelations = relationUpdated;
+
+    return data;
   } catch (error) {
     return new Error(errorsProvider.updateMessage('Sales'));
   }
